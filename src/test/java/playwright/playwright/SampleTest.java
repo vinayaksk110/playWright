77ea8253b0a465bbb180b0ec5d8ee23a6763d932
Vinayak <Vinayak@Vinayak>
2025-04-04 20:19:25 +0530

    first commit

---
+++ src/test/java/playwright/playwright/SampleTest.java
@@ -0,0 +1,18 @@
+package playwright.playwright;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class SampleTest {
+	
+	public static void main(String[] args) {
+		try (Playwright playwright = Playwright.create()) {
+			Browser browser = playwright.chromium().launch();
+			Page page = browser.newPage();
+			page.navigate("http://www.browserstack.com/user/login");
+			System.out.println(page.title());
+			}
+	}
+
+}
