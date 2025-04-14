84a6514470b65528b643a53d9ba71c76d6893763
Vinayak <Vinayak@Vinayak-Dev>
2025-04-14 21:09:09 +0530

    Added log4j2

---
+++ src/main/java/TestBase/Testbase.java
@@ -0,0 +1,11 @@
+package TestBase;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class Testbase {
+	// Logger
+	public static Logger log = LogManager.getLogger(Testbase.class.getName());
+
+}
